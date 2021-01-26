9e1552cb5df730575b66dcaaced1d0f7e32d4f3c
galinamiteva <70331948+galinamiteva@users.noreply.github.com>
2021-01-26 15:20:06 +0100

    commit

--- src/AndraForm.js
+++ src/AndraForm.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import {total} from './ForstForm'
+
 
 function AndraForm(props){
-
-    let divideTotal=(total, friends)=>{
-        const sumDivided=total / parseInt(friends)
-        return sumDivided;
-    };
-
-    let calculateTip = (sum, tip) => {   //бакшиша е почти винаги 10% от общатата сума 
-
-        const calculatedTip = parseInt(sum) * parseFloat(tip);  //тук смятат общата сума умн по бакшиша
-    
-        return calculatedTip;
-    }
-
-    let calcSum = (sum, friends, tip) =>{
-        const calculatedTip= calculateTip(sum, tip);    //решават какъв % бакшиш ще дадат и пресмятат колко е само бакшиша
-        const total = parseInt(sum) + calculatedTip;   // събират сумата+бакшиша
-        const friendSum = divideTotal(total, friends);  //делят (сума+бакшиш)/бр приятели
-        return friendSum;
-    }
-
-    const total=calcSum(sum, friends, tip);
-
-
+    function close(){
+        props.close(props.friendSum)
+   }
 
     return(
         <article>
-            <a href="#" class="back" onClick={ () => close('') }>X</a>
+            <a href="#" className="back" onClick={ close }>X</a>
             <h2>Varje person ska betala</h2>
-            <h3 id='friendSum' className='show-sum'>{total}</h3>
+            <h3 id='friendSum' className='show-sum'>{props.friendSum}</h3>
 
         </article>
     )
--- src/App.js
+++ src/App.js
@@ -4,43 +4,61 @@ import './App.css';
 import AndraForm from './AndraForm';
 
 function App(props) {
-    const [summa, setSumma]=useState(sum='', tip='', friends='');
+    const [sum, setSum]=useState('');
+    const [tip, setTip]=useState('');
+    const [friends, setFriends]=useState('')
+    const [result, setResult]=useState('')
 
-    handleChange(event){
-        const {sum, tip, friends}=event.target;
-        let sum=setSumm({ [sum]: value });
-        let tip=setSumm({ [tip]: value });
-        let friends=setSumm({ [friends]: value });        
+    let divideTotal=(total, friends)=>{
+        const sumDivided=total / parseInt(friends)
+        return sumDivided;
     };
 
-   
+    let calculateTip = (sum, tip) => {   //бакшиша е почти винаги 10% от общатата сума 
+
+        const calculatedTip = parseInt(sum) * parseFloat(tip);  //тук смятат общата сума умн по бакшиша
+    
+        return calculatedTip;
+    }
+
+        function handleClick(){
+            const calculatedTip=calculateTip(sum, tip);
+            const totalSum=parseInt(sum) + calculatedTip;
+            const sumToPay=divideTotal(totalSum, friends)
+            setResult(sumToPay)
+            
+        }
+
+        function comeBack(){
+            setResult('')
+        }
+      
     return(
         <main className='wrapper'>
-            <h2>Split the nota</h2>
-            
-            {summa? 
+            <h2>Split the nota</h2>           
 
+        {result? 
             <section className='form' id='inputForm'>
-            <section>
-                <input type='text' id='sum' className='form_input' value={sum} onChange={handleChange}></input>
-                <label for='sum' className='form_label form_label_sum'>Summa</label>
-            </section>
-            <section>
-                <input type='text' id='numberOfFriends' className='form_input' value={friends} onChange={handleChange} ></input>
-                <label for='numberOfFriends' className='form_label form_label_friends'>Antal vänner</label>
-            </section>
-            <section>
-            <input type='text' id='tip' className='form_input' value={tip} onChange={handleChange}></input>
-                <label for='tip' className='form_label form_label_tip'>Drics</label>
-            </section>
-                <button id='calculateButton' className='form_button' onClick={()=> moreInfo(info)}>Räkna</button>           
-    </section>
-                                 
-            :
-
+                <section>
+                    <input type='number' id='sum' className='form_input' onChange={event => setSum(event.target.value)}></input>
+                    <label for='sum' className='form_label form_label_sum'>Summa</label>
+                </section>
+                <section>
+                    <input type='number' id='numberOfFriends' className='form_input'  onChange={event => setFriends(event.target.value)} ></input>
+                    <label for='numberOfFriends' className='form_label form_label_friends'>Antal vänner</label>
+                </section>
+                <section>
+                <input type='number' id='tip' className='form_input' onChange={event => setTip(event.target.value)}></input>
+                    <label for='tip' className='form_label form_label_tip'>Drics</label>
+                </section>
+                    <button id='calculateButton' className='form_button' onClick ={ handleClick }>Räkna</button>           
+            </section>:                               
+           
             <section className='form' id='showSum'>
-                <AndraForm info={summa} close={setSumma}/> 
-            </section>}
+                <AndraForm friendSum={result} close={comeBack} /> 
+            </section>
+
+            }   
         </main>
     )
 }
